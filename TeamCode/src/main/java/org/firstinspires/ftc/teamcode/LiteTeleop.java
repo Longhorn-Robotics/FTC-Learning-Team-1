55a99a56ecd331cb277de89ea1df91d4517ad114
AdvayRoongta <roongtaadvay@gmail.com>
2025-04-03 16:39:15 -0700

    made linear rail and arm work - game ready

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiteTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiteTeleop.java
@@ -19,8 +19,8 @@ public class LiteTeleop extends OpMode {
     static double RAIL_MIN = 0.0f;
     static double RAIL_MAX = 3000.0f;
 
-    static double ARM_MIN = 0.0f;
-    static double ARM_MAX = 0.8f;
+    static double ARM_MIN = 0.35f;
+    static double ARM_MAX = 0.875f;
 
     static double CLAW_OPEN = 0.125f;
     static double CLAW_CLOSED = 0.25f;
@@ -28,20 +28,19 @@ public class LiteTeleop extends OpMode {
     final double joystickBaseSpeed = 1f;
     
     double railPos = 0.0f;
-    double armPos = 0.0f;
+    double armPos = 0.875f;
     double targetPos;
-    double fullRot = 2786.2;
-
+    double fullRot = 6900; //1988 use   divide by 4.5
     boolean clawOpen = true; //true = OPEN; false = CLOSED
 
     // Code to run ONCE when the driver hits INIT
     @Override
     public void init() {
         robot.init(hardwareMap);
-        robot.arm.setPosition(0.5); //set away from default of zero so it doesnt hit the table
+        robot.arm.setPosition(armPos); //set away from default of zero so it doesnt hit the table
 
         // Send telemetry message to signify robot waiting
-        telemetry.addData("Say:", "Hello Arman-Test");
+        telemetry.addData("Say:", "Hello SIGMA");
     }
 
     // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
@@ -65,9 +64,9 @@ public class LiteTeleop extends OpMode {
 
         // LINEAR RAIL
         if (gamepad1.right_trigger > 0.1f) {
-            encoder(1, 0.2, true);
+            encoder(1, 0.3, true);
         } else if (gamepad1.left_trigger > 0.1f) {
-            encoder(1,0.2, false);
+            encoder(0,0.39, false);
         }
 
         if (railPos < RAIL_MIN) {
@@ -85,7 +84,7 @@ public class LiteTeleop extends OpMode {
             armPos -= 0.05;
         }
 
-        if (armPos < ARM_MIN) {
+       if (armPos < ARM_MIN) {
             armPos = ARM_MIN;
         }
 
@@ -105,14 +104,16 @@ public class LiteTeleop extends OpMode {
         }
 
         if (clawOpen) {
+            telemetry.addData("Debug: ", "CLAW OPEN");
             robot.claw.setPosition(CLAW_OPEN); //45 deg
         } else {
+            telemetry.addData("Debug: ", "CLAW CLOSED");
             robot.claw.setPosition(CLAW_CLOSED); //90 deg
         }
 
 
         // UPDATE POSITION
-        robot.arm.setPosition(armPos);
+       robot.arm.setPosition(armPos);
 
         telemetry.update();
     }
