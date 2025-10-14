39381200de4f1c3399aa2847b832a9e725b24525
andreitran <andrei21tran@gmail.com>
2025-10-14 16:51:55 -0700

    fixed xml file but still errors w motor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -1,28 +1,40 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="Motor Test", group="Testing")
-public class MotorTest extends OpMode {
+@TeleOp(name = "Motor Test", group = "Testing")
+public class MotorTest extends LinearOpMode {
+
+    private DcMotor motor;
 
     @Override
-    public void init() {
+    public void runOpMode() {
+        // Map the motor connected to port 1 (labeled "motor_1" in the Control Hub config)
+        //CHANGE TO "andrei" for device name if it still doesnt work
+        motor = hardwareMap.get(DcMotor.class, "motor_1");
+
         telemetry.addData("Status", "Ready to start");
-    }
+        telemetry.update();
 
-    @Override
-    public void start() {
-        telemetry.addData("Status", "Running");
-    }
+        // Wait for the play button on the Driver Station
+        waitForStart();
 
-    @Override
-    public void loop() {
-        // Keeps updating while the OpMode is running
-    }
+        // Run motor at 20%
+        motor.setPower(0.2);
 
-    @Override
-    public void stop() {
-        telemetry.addData("Status", "Stopped");
+        telemetry.addData("Motor", "Running");
+        telemetry.update();
+
+        // Keep running until stop is pressed
+        while (opModeIsActive()) {
+            // You can also stop the motor if you want to add conditions
+        }
+
+        // Stop the motor when the opmode ends
+        motor.setPower(0);
+        telemetry.addData("Motor", "Stopped");
+        telemetry.update();
     }
 }
